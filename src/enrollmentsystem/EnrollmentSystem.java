e9dc81c9c1b2ae083c4f4903e1bce626dc2c47d3
Louie <Louie@192.168.1.12>
2018-10-10 23:18:15 +0800

    Add a loginform.java to test a local database using a login form Notes: admin , admin for username and fassword

---
+++ src/enrollmentsystem/EnrollmentSystem.java
@@ -0,0 +1,12 @@
+package enrollmentsystem;
+
+public class EnrollmentSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
